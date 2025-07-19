3ab31284583efa6779b1ec2eac79563dcb49f477
peturik <peturik@gmail.com>
2025-07-19 23:10:23 +0200

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
 
+// export default config;
+const config = { plugins: { "@tailwindcss/postcss": {} } };
 export default config;
